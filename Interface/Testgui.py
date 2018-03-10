35382cc11d33f9ee99ab6fbf2a2d32f42fc99eb8
Eatonville Robotics <eatonvillerobotics@gmail.com>
2018-03-09 21:12:29 -0800

    Tested background image

--- Interface/Testgui.py
+++ Interface/Testgui.py
@@ -1,6 +1,10 @@
 import tkinter as tk
+import tkfont
 root =tk.Tk()
-
+C = Canvas(top, bg="blue", height=250, width=300)
+filename = PhotoImage(file = "C:\Users\Cyber Goats\Documents\GitHub\EHS-Control-Systems\Interface\bg.jpg")
+background_label = Label(top, image=filename)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 w = tk.Label(root, text="Python is dumb")
 w.pack()
 
