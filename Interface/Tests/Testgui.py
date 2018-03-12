961b0c0fa8377170c816be44649f3c80d2a983ce
Eatonville Robotics <eatonvillerobotics@gmail.com>
2018-03-11 22:46:41 -0700

    Testing another possible layout.

--- Interface/Tests/Testgui.py
+++ Interface/Tests/Testgui.py
@@ -1,20 +1,18 @@
 import sys
 from tkinter import *
-root = Tk()
-#external code
-fm = Frame(root)
-Button(fm, text='Top').pack(side=TOP, anchor=W, fill=X, expand=YES)
-Button(fm, text='Center').pack(side=TOP, anchor=W, fill=X, expand=YES)
-Button(fm, text='Bottom').pack(side=TOP, anchor=W, fill=X, expand=YES)
-fm.pack(side=LEFT, fill=BOTH, expand=YES)
-fm2 = Frame(root)
-Button(fm2, text='Left').pack(side=LEFT)
-Button(fm2, text='This is the Center button').pack(side=LEFT)
-Button(fm2, text='Right').pack(side=LEFT)        
-fm2.pack(side=LEFT, padx=10)
-frame = Frame(root, width=1500, height=900, bg="black")
-frame.pack()
-root.title("Eatonville Robotics ROV Control System")
+from tkinter import ttk
+control = Tk()
+telemetry = Tk()
+frame = ttk.Frame(telemetry)
+frame = ttk.Frame(control)
 
+#external code
+controlFrame = Frame(control, width=1500, height=900, bg="black")
+telemetryFrame = Frame(telemetry, width=300, height=150, bg="black")
+controlFrame.pack()
+telemetryFrame.pack()
+control.title("EHS ROV - Control System")
+telemetry.title("EHS ROV - Telemetry")
 #end code
-root.mainloop()
+control.mainloop()
+telemetry.mainloop()
