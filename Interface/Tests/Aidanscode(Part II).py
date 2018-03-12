00a6342474349f289bca3ee844da0292e7f8c45d
Husky797 <aidanames301@gmail.com>
2018-03-12 09:50:39 -0700

    File Fix

--- Interface/Tests/Aidanscode(Part II).py
+++ Interface/Tests/Aidanscode(Part II).py
@@ -4,7 +4,7 @@ from PIL import ImageTk
 canvas = Canvas(width = 3840, height = 2160, bg = 'blue')
 canvas.pack(expand = YES, fill = BOTH)
 
-image = ImageTk.PhotoImage(file = "C:/ThisPC/Desktopbg.gif")
+image = ImageTk.PhotoImage(file = "C:/ThisPC/Desktop/RoboticsCrap/bg.gif")
 canvas.create_image(10, 10, image = image, anchor = NW)
 
 mainloop()
